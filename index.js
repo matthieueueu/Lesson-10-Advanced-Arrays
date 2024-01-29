ff9d61eccec4b2f17bc64b75db32191416dbfa6b
Matt MIC <matthieumichelet99@gmail.com>
2024-01-29 08:30:45 +0200

    new changes 29.01.24 08:30

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const print = console.log;
 
 let numbers = [1,2,3,4,5,6,7,8,9]
 
+
+// pop() pop off the last
+// push() adds to back of array
+// unshift() adds to front of array
+// filter() filters out elements of array
+// slice() removes element(s) of index
+// map() manipulates and store as new array
+// forEach() does a function for each array
+
+
+
+
 numbers.pop();
 numbers.push(632);
 numbers.unshift("jamaica");
