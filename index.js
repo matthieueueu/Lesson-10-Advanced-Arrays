4e0a2b90984ec728b35c8c0e82efa9ed86496fe4
Matt MIC <matthieumichelet99@gmail.com>
2024-02-01 14:29:18 +0200

    new challenge added 01.02.24  14:29

--- index.js
+++ index.js
@@ -43,11 +43,10 @@ let numbers = [1,2,3,4,5,6,7,8,9]
 //CHALLENGE
 
 const numberList = [1,2,3,4,5]
-print(numberList)
+
 
 function removeMiddleAddFive(array1){
     let getFirstNum = array1.slice(0);
-    print(getFirstNum)
     let getSecondNum = array1.slice(-1);
 
     let newArray = [getFirstNum[0],5,getSecondNum[0]]
@@ -57,4 +56,86 @@ function removeMiddleAddFive(array1){
 removeMiddleAddFive(numberList);
 
 
+//FOREACH()
+print(numberList)
+numberList.forEach(addToConsoleAddOne);
 
+
+function addToConsoleAddOne(element){
+    print(element+1);
+}
+print("");
+
+numberList.forEach(function (element){
+    element -=2;
+    print(element*5);
+});
+
+
+
+
+
+//FILTER()
+
+print("")
+
+const filteredArray = numberList.filter(overThree);
+print(filteredArray);
+
+function overThree(element){
+    return element > 3;
+}
+
+print("")
+const filteredArray1 = numberList.filter(onlyTwo);
+print(filteredArray1);
+
+function onlyTwo(element){
+    return element == 2;
+}
+
+//CHALLENGE
+print("")
+const drink = ["vodka","tequila","kool-aid","capri-sun","schnapps","horse juice","blended kashmere sweaters"];
+
+drink.forEach(function(element){
+    print("i love drinking "+element);
+})
+
+
+const newArray=[];
+const arrayLessThanFour = numberList.forEach(function(element){
+    console.log('comparing: ', element)
+    if(element < 4){
+        console.log(element + ' is less than 4')
+        newArray.push(element);
+    }
+})
+
+print(newArray)
+
+//CHALLENGE
+print("")
+
+// create new array with the doubles of the other numbers.
+// create a for loop to add all numbers in the new array then output new num 
+
+
+const randomNumArray = [30,444,1908,65,1100009,99900];
+doubledNums = []
+
+const numbersDoubled = randomNumArray.forEach(function(element){
+    element = element*2;
+    doubledNums.push(element)
+})
+
+print(doubledNums); 
+
+const newArrayLength = doubledNums.length;
+let total = 0;
+
+for(var i=0;i<newArrayLength;i++){
+    total += doubledNums[i];
+}
+
+print(total)
